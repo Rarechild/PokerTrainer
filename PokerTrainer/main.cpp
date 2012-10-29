8ad8564435937b6802959fd5b9ba47d94c235b34
Matt Giannetto <github@giannet.to>
2012-10-29 16:03:27 -0400

    Initial Commit

---
+++ PokerTrainer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PokerTrainer
+//
+//  Created by Fairchild on 10/29/12.
+//  Copyright (c) 2012 Matt Giannetto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
